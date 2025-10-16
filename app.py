5ab435c4c89157b5d35d6c2a8117c402769dcca1
Kaush <superkaush@gmail.com>
2025-10-15 19:26:46 -0700

    pywebview integration

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-import pywebview
+import webview
+from pathlib import Path
+
+app_path = (Path(__file__).parent / "src" / "index.html").resolve()
+
+webview.create_window(
+    "Nomad",
+    app_path.as_uri(),
+    width=1200,
+    height=800,
+    resizable=True
+)
+
+
+webview.start()
