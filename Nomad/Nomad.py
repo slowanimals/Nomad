1c8e5b2993419a5edd089ce664ea0a52da29ab82
Kaush <superkaush@gmail.com>
2025-10-20 19:51:16 -0700

    html embed

--- Nomad/Nomad.py
+++ Nomad/Nomad.py
@@ -19,11 +19,17 @@ def index():
         rx.hstack(
             #map
             rx.box(
-                rx.html("<iframe src='/themap.html'></iframe>")
+                rx.html("<iframe src='/themap.html'></iframe>"), height = "500px", width = "100%"
             ),
             #dashboard
-            rx.box("dashboard area", bg="gray.100", width="40%"),
+            rx.box("dashboard area", bg="gray.100", width="30%"),
+            height = "100vh",
+            width = "100%",
+            align_itmes = "stretch"
         ),
+        height = "100vh",
+        width = "100%",
+
     )
 
 app = rx.App()
