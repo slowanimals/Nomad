ba4747e55a07329c1ef77ef441a610e54929d5de
Kaush <superkaush@gmail.com>
2025-10-22 15:32:37 -0700

    logo edit

--- Nomad/Nomad.py
+++ Nomad/Nomad.py
@@ -27,7 +27,7 @@ def index():
                 margin_top = '10px',
                 margin_left = '1.5%',
                 border_radius = '30px',
-                box_shadow = '10px 10px 50px 5px var(--gray-11), -10px -10px 50px 5px var(--gray-11)',
+                box_shadow = '10px 10px 50px 3px var(--gray-10), -10px -10px 50px 5px var(--gray-11)',
 
             ),
             
@@ -37,17 +37,18 @@ def index():
                 rx.box(
                     rx.text("Nomad", 
                             font_family = 'Bytesized', 
-                            color = "black",
+                            color = "white",
                             size = '9',
                             ),
-                    bg = "var(--bronze-12)",
+                    bg = "var(--mauve-5)",
                     width = "50%",
                     padding = "3px",
-                    margin_top = "5px",
-                    border = "4px solid var(--gold-8)",
-                    border_radius = "30px",
+                    margin_top = "7px",
+                    #border = "4px solid var(--gray-8)",
+                    border_radius = "20px",
                     style = {'user-select': 'none', 'font-size' : '5vw'},
-                    _hover={'cursor':'pointer', 'font-weight':'bold'}
+                    _hover={'cursor':'pointer', 'font-weight':'bold'},
+                    box_shadow = '3px 4px 60px 3px var(--gray-9), inset 0.3px 0.3px 2px white, inset -0.3px -0.3px 2px white',
                     
                 ),
                 width = "30%",
@@ -60,14 +61,14 @@ def index():
                 margin_bottom = "10px",
                 height = "97vh",
                 bg = 'var(--mauve-11)',
-                #box_shadow = '10px 10px 50px 5px gray, -10px -10px 50px 0px gray'
                 _hover = {'cursor':'pointer'},
-                box_shadow = '12px 15px 30px 4px var(--mauve-7)',
+                box_shadow = '12px 15px 30px 4px var(--mauve-7), inset 0.5px 1px 3px white, inset -0.5px -1px 2px white',
+                overflow_y = 'auto',
                 
             ),
             text_align = "center",
             justify = "end",
-            bg = "var(--mauve-9)",
+            bg = "var(--mauve-8)",
             height = "100vh",
             width = "100%",
             align = 'center',
@@ -81,7 +82,7 @@ def index():
 
 app = rx.App(
     stylesheets=[
-        "https://fonts.googleapis.com/css2?family=Bytesized&family=Coral+Pixels&family=Jersey+15&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+        "https://fonts.googleapis.com/css2?family=Bytesized&family=Coral+Pixels&family=Jacquard+12&family=Jersey+15&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap",
     ],
 )
 app.add_page(index)
