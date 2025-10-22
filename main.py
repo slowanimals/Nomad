2c8230269e47ad4c3fd3791e2cb58daa87cb1863
Kaush <superkaush@gmail.com>
2025-10-21 22:52:09 -0700

    automated map rendering

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import render
 import folium
-
+from pathlib import Path
+import os
 def run(base_map, filename, color):
     try:
         render.plot(base_map,filename, color)
     except UserWarning:
         render.plot(base_map,filename,'purple')
-    base_map.save('multimap2.html')
+    base_map.save('assets/multimap2.html')
 
 map = folium.Map(location = (34.0556, -117.1825), 
                  zoom_start = 4, 
@@ -17,6 +18,11 @@ map = folium.Map(location = (34.0556, -117.1825),
                  max_bounds=True
                  )
 
-run(map,'alaska','purple')
-run(map,'yellowstone','blue')
-run(map,'hawaii','red')
+trips = Path(__file__).parent.resolve() / "Trips"
+
+count = 0
+colors = ['purple', 'blue', 'red', 'green']
+
+for f in trips.iterdir():
+    run(map,f,colors[count])
+    count += 1
