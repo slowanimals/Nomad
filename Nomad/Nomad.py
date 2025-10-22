eb4f8d181331a46331de0dc90df04281b31c4979
Kaush <superkaush@gmail.com>
2025-10-22 14:41:21 -0700

    ui 50%

--- Nomad/Nomad.py
+++ Nomad/Nomad.py
@@ -12,23 +12,68 @@ class State(rx.State):
 
 def index():
     return rx.box(
-        rx.vstack(
-        #title
+        rx.hstack(
+            #map
             rx.box(
-                rx.text("Nomad", font_family = 'Bytesized', size = '9', color = "black"),
-                bg = "var(--bronze-12)",
-                width = "100%",
-                padding = "10px",
-                border_bottom = "4px solid var(--gold-8)",
+                rx.html("<iframe src='/themap.html' height='100%' width='100%' style='border:none; border-radius: 27px;'></iframe>",
+                        height = '100%',
+                        width = '100%',
+                        display = 'block',
+                        ),
+
+                #border = '4px solid black',
+                width = '70%',
+                height = '90vh',
+                margin_top = '10px',
+                margin_left = '1.5%',
+                border_radius = '30px',
+                box_shadow = '10px 10px 50px 5px var(--gray-11), -10px -10px 50px 5px var(--gray-11)',
+
+            ),
+            
+            #dashboard
+            rx.vstack(
+            #title
+                rx.box(
+                    rx.text("Nomad", 
+                            font_family = 'Bytesized', 
+                            color = "black",
+                            size = '9',
+                            ),
+                    bg = "var(--bronze-12)",
+                    width = "50%",
+                    padding = "3px",
+                    margin_top = "5px",
+                    border = "4px solid var(--gold-8)",
+                    border_radius = "30px",
+                    style = {'user-select': 'none', 'font-size' : '5vw'},
+                    _hover={'cursor':'pointer', 'font-weight':'bold'}
+                    
+                ),
+                width = "30%",
+                #border = "4px solid black",
+                border_radius = "30px",
+                align = "center",
+                margin_right = "13px",
+                margin_left = "10px",
+                margin_top = "10px",
+                margin_bottom = "10px",
+                height = "97vh",
+                bg = 'var(--mauve-11)',
+                #box_shadow = '10px 10px 50px 5px gray, -10px -10px 50px 0px gray'
+                _hover = {'cursor':'pointer'},
+                box_shadow = '12px 15px 30px 4px var(--mauve-7)',
+                
             ),
             text_align = "center",
-            align = "center",
-        
+            justify = "end",
+            bg = "var(--mauve-9)",
+            height = "100vh",
+            width = "100%",
+            align = 'center',
             
         ),
-        bg = "var(--sand-2)",
-        height = "100vh",
-        width = "100%"
+        
         
     ),
    
